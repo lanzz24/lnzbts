9841c3fa53789296d0dd2c1cbf534b00ebd60248
lanzz24 <88968605+lanzz24@users.noreply.github.com>
2021-08-20 01:52:09 +0700

    Update start.js

--- modules/start.js
+++ modules/start.js
@@ -10,7 +10,7 @@ async function get(battery, phn_info) {
     }
 
     return ({
-        msg: `*Whatsbot* _(1.5.0)_\n\nThis chat is Powered By *FadhlanBots*\n\n*Battery:* ${batttxt}\n*Device:* ${phn_info.device_manufacturer} ${phn_info.device_model}\n*WA Version:* ${phn_info.wa_version}\n*Pmpermit:* ${config.pmpermit_enabled}\n*Mutetime:* ${config.pmpermit_mutetime/60} Minutes\n\n*Official Repository Url 👇*\n` + "```https://github.com/lanzz24/lnzbts```",
+        msg: `*FADHLAN BOTS* _(1.8, 0)_\n\nThis chat is Powered By *FadhlanBots*\n\n*Battery:* ${batttxt}\n*Device:* ${phn_info.device_manufacturer} ${phn_info.device_model}\n*WA Version:* ${phn_info.wa_version}\n*Pmpermit:* ${config.pmpermit_enabled}\n*Mutetime:* ${config.pmpermit_mutetime/60} Minutes\n\n*Official Repository Url 👇*\n` + "```https://github.com/lanzz24/lnzbts```",
         mimetype: "image/jpeg",
         data: Buffer.from(((await axios.get('https://i.ibb.co/jTKG9SC/IMG-20210812-121619-261.jpg', { responseType: 'arraybuffer' })).data)).toString('base64'),
         filename: "start.jpg"
